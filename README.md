d598a0c0bfcad525852586119cba331448987fca
thulasiram <thulasiram.ponnam@in.verizon.com>
2018-06-21 12:30:03 +0530

    safarionline video course offline downloader in python

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # safarionlinevideo_downloader
+Safari Video downloader is a python script to download entire course video's for offline watching.
+
+
+
+Pre requisites:
+ * Linux Machine
+ * Install Python3
+ * Install youtube_dl
+ 
+ Usage:
+   * open config.json file and update required details like username and password
+     {
+        "url": "https://www.safaribooksonline.com/library/view/ultimate-go-programming/9780134757476/",
+        "domain": "https://www.safaribooksonline.com",
+        "output_folder": "./output",
+        "username": "username",
+        "password": "password"
+    }
+
+   * run the following command in shell
+     python3 safariVideoDownloader.py
+
+ Note:
+ * User required valid account on https://www.safaribooksonline.com/
+
+ References:
+ https://www.python.org/downloads/
+ https://rg3.github.io/youtube-dl/
---
+++ safariVideoDownloader.py
@@ -0,0 +1,57 @@
+
+from bs4 import BeautifulSoup
+import requests
+import shutil
+import os
+import subprocess
+import json
+
+with open('./config.json') as f:
+    data = json.load(f)
+url = data['url']
+domain = data['domain']
+output_folder = data['output_folder']
+username = data['username']
+password = data['password']
+
+req = requests.get(url)
+soup = BeautifulSoup(req.text, 'html.parser')
+
+lessons = soup.find_all('li', class_='toc-level-1')
+print(len(lessons))
+
+shutil.rmtree(output_folder, ignore_errors=True)
+os.makedirs(output_folder)
+module_name = 'Module 0'
+for lesson in lessons:
+    lesson_name = lesson.a.text
+    if lesson_name.startswith('Module') and not 'Summary' in lesson_name:
+        module_name = lesson_name
+        os.makedirs(output_folder + '/' + module_name)
+        # print(module_name)
+        for index, video in enumerate(lesson.ol.find_all('a')):
+            video_name = str(index) + ' - ' + video.text
+            video_url = video.get('href')
+            video_out = output_folder + '/' + module_name + '/' + video_name + '.mp4'
+            #print('        ', domain + video_url)
+            #print('        ', video_out)
+            url_opath = "youtube-dl -f 133 -u {} -p {} --output '{}' -f worst {}".format(
+                username, password, video_out, video_url)
+            print(url_opath)
+            #print("youtube-dl -u {} -p {} --output '{}' {}".format(username, password, video_out, video_url))
+           # subprocess.run(url_opath, shell=True, check=True)
+    else:
+        os.makedirs(output_folder + '/' + module_name + '/' + lesson_name)
+        # print('   ', lesson_name)
+        for index, video in enumerate(lesson.ol.find_all('a')):
+            video_name = str(index) + ' - ' + video.text
+            video_url = video.get('href')
+            video_out = output_folder + '/' + module_name + \
+                '/' + lesson_name + '/' + video_name + '.mp4'
+            # print('        ', domain + video_url)
+            # print('        ', video_out)
+            url_opath = "youtube-dl -u {} -p {} --output '{}' -f worst {}".format(
+                username, password, video_out, video_url)
+            print(url_opath)
+            #print("youtube-dl -u {} -p {} --output '{}' {}".format(username, password, video_out, video_url))
+           # subprocess.run(url_opath, shell=True, check=True)
